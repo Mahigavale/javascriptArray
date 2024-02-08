ae55b2de28fba256ff80ea59a25238715be97c0e
Mahigavale <maheshgavale07@gmail.com>
2024-02-08 17:52:24 +0530

    more objects in index

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello.....");
+ name="ashok";
+
+const person1={
+name:"mahesh",
+age:23,
+city:"Pune",
+area:"hadapsar",
+pincode:123456,
+status:true,
+
+getalldetails:
+function(){
+    return "name :"+this.name+" age :"+this.age+" area :"+this.area+" city :"+this.city+" status :"+this.status ;
+}
+}
+
+console.log(person1.name);
+console.log(person1.getalldetails());
+let bb=person1.getalldetails();
+console.log(bb);
+
+function myfunction()
+{
+    let name="mahesh";
+    console.log(name);
+    console.log(this.name);
+}
+
+myfunction();
