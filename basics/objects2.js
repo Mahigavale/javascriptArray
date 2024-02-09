b6bac30432ab23113b09fc3702144f9be791c94f
Mahigavale <maheshgavale07@gmail.com>
2024-02-09 22:56:25 +0530

    more on the js

---
+++ basics/objects2.js
@@ -0,0 +1,36 @@
+
+const person={
+    name:"mahesh",
+    email:"maheshgavale07@gmail.com"
+}
+const person2={
+    name:'adarsh',
+    email:'maheshgavale07@gmail.com',
+    number:1234567890,
+    state:'maharashtra'
+
+}
+ vd='mahesh';
+console.table(person);
+console.table(person2);
+console.log(typeof(person2.name));
+console.log(typeof(vd))
+
+var demo=new Array();
+demo=[12,23,45,56,56,'mahesh']
+//in js array can store different values of different data types.
+console.log(demo[2])
+console.log(typeof(demo[2]))
+console.log(typeof(demo[5]))
+console.log(demo);
+console.table(demo);
+console.log(typeof(demo))
+demo[6]=["ashok",34]
+
+console.log("Final Array:",demo);
+console.table(demo); 
+
+
+let arr=[1,2,3,4]
+ let arr1=[1,2,]
+op=[1,2,6,10]
