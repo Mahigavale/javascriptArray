10e4bb3126fc131810edc821d6ad9c61b1f9de2b
Mahigavale <maheshgavale07@gmail.com>
2024-02-18 01:35:39 +0530

    splice method in java

---
+++ splice.js
@@ -0,0 +1,16 @@
+// /*
+// splice is used to modify and and add or remove the elements from
+// the original array and returns the original array.
+// */
+
+// let arr=['abc','def','ghi','jkl','mno','pqr','stu','vwx'];
+
+// console.log(arr);
+// console.log(arr.splice(0,1,'xyz'));
+// console.log(arr);
+
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+console.log(arr);
+arr.splice(1,1,11,12,13,14,15);
+console.log(arr);
